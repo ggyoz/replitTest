02d14bc53b41e783b67f474e379846b76b1a790d
ggyoz <34002658+ggyoz@users.noreply.github.com>
2022-07-15 15:02:36 +0000

    LeetCode 26. 문제풀이 (수정)

--- LeetCode/removeDuplicates2.py
+++ LeetCode/removeDuplicates2.py
@@ -1,16 +1,27 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:        
         
-        newnums = list()
-        newnums.append(nums[0])        
+        # newnums = list()
+        # newnums.append(nums[0])        
         
-        for n in range(1, len(nums)):
+        # for n in range(1, len(nums)):
             
-            if nums[n-1] != nums[n]:
-                newnums.append(nums[n])
+        #     if nums[n-1] != nums[n]:
+        #         newnums.append(nums[n])
         
-        nums = newnums
+        # nums = newnums
         
-        print(nums)
+        # print(nums)
+
+        num = ''
+        tmp = nums[:]
+        
+        for n in tmp:
+            
+            if n == num:
+                nums.remove(n)
+            num = n               
+        
+        return len(nums)
         
         
