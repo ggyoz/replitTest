b1fc0f91187cff0801c6db604d3cf09a7693a75f
ggyoz <34002658+ggyoz@users.noreply.github.com>
2022-07-27 15:14:43 +0000

    114. Flatten Binary Tree to Linked List 다른풀이 추가

--- LeetCode/flatten.py
+++ LeetCode/flatten.py
@@ -49,4 +49,22 @@ class Solution:
             return r or l or root
         
         return dfs(root)
+
+
+# 다른 풀이
+# class Solution:    
+#     def __init__(self):
+#         self.prev=None
+        
+#     def flatten(self, root: Optional[TreeNode]) -> None:
+        
+#         if not root:            
+#             return None
+        
+#         self.flatten(root.right)
+#         self.flatten(root.left)
+#         #print(root.right)
+#         root.right=self.prev
+#         root.left=None
+#         self.prev=root
         
