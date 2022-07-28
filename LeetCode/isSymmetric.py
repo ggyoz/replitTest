f2c1b7194b4ba895aeb2d0da9436208878b8d9a8
ggyoz <34002658+ggyoz@users.noreply.github.com>
2022-07-28 14:07:35 +0000

    101. Symmetric Tree BFS 문제풀이

--- LeetCode/isSymmetric.py
+++ LeetCode/isSymmetric.py
@@ -1,42 +1,27 @@
 class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-
-        # DFS 방식
-        def dfs(l, r):
-            
-            if not root:
-                return True
-            
-            if not (l and not r):
-                return True
-            if not (l or not r):
-                return False
-            
-            return l.val == r.val and dfs(l.left, r.right) and dfs(l.right, r.left)
-        
-        return dfs(root.left, root.right)
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:        
       
         # BFS 방식
-        # q = collections.deque([root])
+        q = collections.deque([root])
 
-        # while q:
-        #     level = []
-        #     size = len(q)
-        #     print(size)
+        while q:
+            level = []
+            size = len(q)
+            print(size)
 
-        #     for i in range(size):
+            for i in range(size):
                 
-        #         node = q.popleft()
-        #         if not node:
-        #             level.append(None)
-        #         else:
-        #             level.append(node.val)
+                node = q.popleft()
+                if not node:
+                    level.append(None)
+                else:
+                    level.append(node.val)
             
-        #             q.append(node.left)
-        #             q.append(node.right)
+                    q.append(node.left)
+                    q.append(node.right)
             
-        #     print(level)
+            print(level)
             
-        #     if level != level[::-1]: return False
+            if level != level[::-1]: return False
             
-        # return True
+        return True
