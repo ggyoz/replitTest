d11badb2f179dff4a5a3441cf404e2147917c85e
ggyoz <34002658+ggyoz@users.noreply.github.com>
2022-07-28 14:04:18 +0000

    101. Symmetric Tree BFS 문제풀이 추가

--- LeetCode/isSymmetric.py
+++ LeetCode/isSymmetric.py
@@ -1,16 +1,42 @@
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        
-        def areMirrorImage(leftNode, rightNode):
-            if (not (leftNode and not rightNode):
+
+        # DFS 방식
+        def dfs(l, r):
+            
+            if not root:
                 return True
-            if (not (leftNode or not rightNode):
+            
+            if not (l and not r):
+                return True
+            if not (l or not r):
                 return False
             
-            return leftNode.val == rightNode.val and areMirrorImage(leftNode.left, rightNode.right) and areMirrorImage(leftNode.right, rightNode.left)
+            return l.val == r.val and dfs(l.left, r.right) and dfs(l.right, r.left)
         
-        
-        if not root:
-            return True
-        
-        return areMirrorImage(root.left, root.right)
+        return dfs(root.left, root.right)
+      
+        # BFS 방식
+        # q = collections.deque([root])
+
+        # while q:
+        #     level = []
+        #     size = len(q)
+        #     print(size)
+
+        #     for i in range(size):
+                
+        #         node = q.popleft()
+        #         if not node:
+        #             level.append(None)
+        #         else:
+        #             level.append(node.val)
+            
+        #             q.append(node.left)
+        #             q.append(node.right)
+            
+        #     print(level)
+            
+        #     if level != level[::-1]: return False
+            
+        # return True
